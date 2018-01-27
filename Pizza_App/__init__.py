c77c6d680123bdce40a4e9b1ebdfdec26bd550d1
Nate <nxr9266@g.rit.edu>
2018-01-27 16:54:59 -0500

    Added my name to ReadMe

---
+++ Pizza_App/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, url_for, request, flash, redirect, session, jsonify
+#from dbconnect import connection
+#from wtforms import Form
+# import json
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    error="Homepage didn't load, SOOO sad"
+    try:
+    	# return "It worked!"
+    	return render_template("index.html")
+    except Exception as e:
+        return print("BRO THIS IS THE ERROR!~!", str(e))
+
+
+if __name__=="__main__":
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+    app.run(debug=True)
