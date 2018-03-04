544cfa383d64a78fe1fc32536c785accc28f3634
nateraw <nxr9266@g.rit.edu>
2018-03-04 08:31:58 -0500

    Data Dictionary now is passed into javascript from Flask

--- Pizza_App/__init__.py
+++ Pizza_App/__init__.py
@@ -11,11 +11,12 @@ def homepage():
 
     if request.method == "POST":
         return redirect(url_for("pizza_submit"))
-
+    data_in = {"Pizza1":[[43.082874, -77.677129]],
+                   "Pizza2":[[43.0843569, -77.6821797]],
+                   "Pizza3":[[43.083541, -77.677354]]}
     error="Homepage didn't load, SOOO sad"
     try:
-    	# return "It worked!"
-    	return render_template("index.html")
+    	return render_template("index.html", data_in=data_in)
     except Exception as e:
         return print("BRO THIS IS THE ERROR!~!", str(e))
 
@@ -24,10 +25,11 @@ def pizza_submit():
     error="Form didn't load, SOOO sad"
     if request.method =="POST":
         return redirect(url_for("homepage"))
-
     try:
-    	# return "It worked!"
-    	return render_template("form.html")
+
+
+        # return "It worked!"
+        return render_template("form.html")
     except Exception as e:
         return print("BRO THIS IS THE ERROR!~!", str(e))
 
